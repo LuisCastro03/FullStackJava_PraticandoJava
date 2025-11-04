10ddb870a9957da9b21814be61b9943302e0ebd5
Luis Castro <luisr.contato@gmail.com>
2025-11-04 11:55:40 -0300

    Add files via upload

---
+++ Praticando Java - variaveis e tipos/src/Atividade4.java
@@ -0,0 +1,16 @@
+//Atividade 4 - Conversão de temperatura
+
+public class Atividade4 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        System.out.println();
+
+        int celsius = 20;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        double conversaoFahrenheit = fahrenheit;
+        System.out.println("A temperatura em graus Fahrenheit é: " + conversaoFahrenheit);
+
+    }
+}
---
+++ Praticando Java - variaveis e tipos/src/Atividade5.java
@@ -0,0 +1,22 @@
+//Atividade 5 - Cadastro de livros
+
+public class Atividade5 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        System.out.println();
+
+        String titulo = "Estrelinha do Saber";
+        String autor = "Selene Chronos Grimm";
+        int paginas = 24;
+        double preco = 9.99;
+
+        if {
+            char categoria =
+        }
+
+        System.out.println("Livro cadastrado: " + titulo + ", de " + autor +
+                ". Ele possui " + paginas + " páginas, custa R$" + preco +
+                "e pertence à categoria")
+    }
+}
